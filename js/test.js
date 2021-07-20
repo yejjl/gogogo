5db73feef813d7bd82e135eaf3d5984d85b4cc9c
yejiale <2499226042@qq.com>
2021-07-20 15:56:13 +0800

    7_20_npmInit_2

--- js/test.js
+++ js/test.js
@@ -1,7 +1,13 @@
-const time = require('./myJS');
+//传统方法
+// const time = require('./myJS');
 
-const dt = new Date();
+// const dt = new Date();
+// console.log(dt);
+
+// const newDT = time.dateFormat(dt);
+// console.log(newDT);
+
+//npm方法
+const moment = require('moment');
+const dt = moment().format('YYYY-MM-DD HH:mm:ss');
 console.log(dt);
-
-const newDT = time.dateFormat(dt);
-console.log(newDT);
