cc1c988902cc2b5c15777709189dcdbbc7d7026a
yejiale <2499226042@qq.com>
2021-07-22 09:44:14 +0800

    7_22_npm publish

--- js/test.js
+++ js/test.js
@@ -1,14 +1,25 @@
 //传统方法
-// const time = require('./myJS');
+const time = require('../yjl_tools');
 
-// const dt = new Date();
-// console.log(dt);
+const dt = new Date();
+console.log(dt);
 
-// const newDT = time.dateFormat(dt);
-// console.log(newDT);
+const newDT = time.dateFormat(dt);
+console.log(newDT);
 
 //npm方法
-const moment = require('moment');
-const dt = moment().format('YYYY-MM-DD HH:mm:ss');
-console.log(dt);
-console.log(typeof moment);
+// const moment = require('moment');
+// const dt = moment().format('YYYY-MM-DD HH:mm:ss');
+// console.log(dt);
+// console.log(typeof moment);
+
+//转义html
+const mytools = require('../yjl_tools');
+const htmlStr = '<h1 title="abc">这是标签<span>123&nbsp;</span></h1>';
+
+const str = mytools.htmlEscape(htmlStr);
+console.log(str);
+
+//还原html
+const str2 = mytools.htmlUnEscape(str);
+console.log(str2);
